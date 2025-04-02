b4a651c1b2255ca4e25de8e97428e250741ff766
superlou <lousimons@gmail.com>
2025-04-01 21:23:41 -0400

    Bad stars

--- consoles/view_screen_console.gd
+++ consoles/view_screen_console.gd
@@ -4,10 +4,12 @@ class_name ViewScreenConsole
 
 @onready var PositionLabel := find_label_by_text("position")
 @onready var grid_shader := $View/MoveReferenceGrid.material_override as ShaderMaterial
+@onready var star_field:MultiMeshInstance3D = $View/StarField
 
 
 func _ready() -> void:
 	ship_net.received_message.connect(_on_ship_net_message)
+	_init_star_field()
 
 
 func _on_ship_net_message(msg):
@@ -16,3 +18,18 @@ func _on_ship_net_message(msg):
 		PositionLabel.text = str(position)
 
 		grid_shader.set_shader_parameter("offset", position * 0.001)
+
+
+func _init_star_field() -> void:
+	var num_stars := 1024
+	star_field.multimesh.instance_count = num_stars
+	for i in range(num_stars):
+		var origin := Vector3(
+			randf_range(-1000, 1000),
+			randf_range(-1000, 1000),
+			randf_range(-1000, 1000)
+		)
+		var transform := Transform3D(Basis.IDENTITY, origin)
+		print(transform)
+		star_field.multimesh.set_instance_transform(i, transform)
+		
